59f69592e8d9e7b3d12c7678be52467c9154c2ea
Гульнара Загарова <n459sverk@gmail.com>
2023-02-12 18:18:02 +0300

    init:test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+    const arr=String(n).split('');
+    if(arr[0]=='-' && arr[arr.length-1]==0 && arr[arr.length-2]!=0){
+      arr.shift();
+      arr.pop();
+    } else
+    if(arr[0]=='-' && arr[arr.length-1]==0 && arr[arr.length-2]==0){
+      arr.shift();
+      arr.pop();
+      arr.pop()
+    }
+    if(arr[0]=='-' && arr[arr.length-1]!=0 && arr[arr.length-2]!=0){
+        arr.shift();
+    }
+    if(arr[0]=='-' && arr[arr.length-1]!=0 && arr[arr.length-2]==0){
+        arr.shift();
+    }
+    const reversedArr=arr.reverse();
+    const reversedNumber=reversedArr.join('');
+    console.log(reversedNumber);
+    return reversedNumber;
 }
